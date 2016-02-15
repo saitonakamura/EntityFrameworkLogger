e4377739bd2a8570618a650ce72b05eb7fce5ab7
Saito Nakamura <saitonakamuramb@gmail.com>
2016-02-16 02:54:36 +0300

    start, not working

---
+++ EntityFrameworkLogger/Model/EntityFrameworkLoggerContext.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace EntityFrameworkLogger.Model
+{
+    public class EntityFrameworkLoggerContext : DbContext
+    {
+        public EntityFrameworkLoggerContext() : base("EntityFrameworkLoggerContext")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Artist> Artist { get; set; }
+        public DbSet<Album> Album { get; set; }
+        public DbSet<Track> Track { get; set; }
+    }
+
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public virtual Artist Artist { get; set; }
+        public virtual ICollection<Track> Tracks { get; set; }
+    }
+
+    public class Track
+    {
+        public int TrackId { get; set; }
+        public string Name { get; set; }
+        public virtual Album Album { get; set; }
+        public string Composer { get; set; }
+    }
+}
---
+++ EntityFrameworkLogger/Tests/Tests.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using EntityFrameworkLogger.Model;
+using NUnit.Framework;
+
+namespace EntityFrameworkLogger.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private IContainer _ioc;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<TestModule>();
+            _ioc = builder.Build();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var testController = _ioc.Resolve<TestController>();
+
+            var artists = testController.GetArtists();
+
+            Assert.IsNotEmpty(artists);
+        }
+    }
+
+    public class TestController
+    {
+        private readonly EntityFrameworkLoggerContext _context;
+
+        public TestController(EntityFrameworkLoggerContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Artist> GetArtists()
+        {
+            return _context.Artist.ToList();
+        }
+    }
+
+    public class TestModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.Register(x => new EntityFrameworkLoggerContext());
+
+            builder.RegisterType<TestController>();
+        }
+    }
+}
